5a8df5bf5ec6862e861f5d219449102af01d2f3a
Mike Akiba <mike2718@gmail.com>
2022-05-26 13:47:24 +0800

    更新

--- archive.bat
+++ archive.bat
@@ -2,6 +2,7 @@
 cd /d "%~dp0"
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,4%_%datetime:~4,2%_%datetime:~6,2%
-del /q save_*.tar.zst 2>nul
-git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -19 --output=../save_%datetime%_HEAD.tar.zst HEAD
+del /q ../save_*.tar.zst 2>nul
+git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -o ../save_%datetime%_HEAD.tar.zst HEAD
 pause
+
