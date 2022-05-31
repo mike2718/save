137e8516e289f9c3fafd1c6c0dfd1c1d3f5f8149
Mike Akiba <mike2718@gmail.com>
2022-06-01 02:26:06 +0800

    更新

--- archive.bat
+++ archive.bat
@@ -2,7 +2,8 @@
 cd /d "%~dp0"
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,4%_%datetime:~4,2%_%datetime:~6,2%
-del /q ../save_*.tar.zst 2>nul
-git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -o ../save_%datetime%_HEAD.tar.zst HEAD
+rem del /q ../save_*.tar.zst 2>nul
+rem git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -o ../save_%datetime%_HEAD.tar.zst HEAD
+RD /S /Q "E:\dev\save\Xeno+"
+XCOPY "E:\game\Xeno+" "E:\dev\save\Xeno+" /s /i
 pause
-
