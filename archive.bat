33a92646cbd73aa2c1e1a59f27c012f2a38d892f
Mike Akiba <mike2718@gmail.com>
2022-06-30 23:26:16 +0800

    使用rar保存归档

--- archive.bat
+++ archive.bat
@@ -1,10 +1,11 @@
 @echo off
 cd /d "%~dp0"
+
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,4%_%datetime:~4,2%_%datetime:~6,2%
 
-del /q ..\save_*.tar.zst 2>nul
-git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -o ../save_%datetime%_HEAD.tar.zst HEAD
+rem del /q ..\save_*.tar.zst 2>nul
+rem git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -o ../save_%datetime%_HEAD.tar.zst HEAD
 
 DEL /Q ".\¹Ö®Ö¢ºòÈº\Save*.lsd" 2>nul
 COPY /Y "E:\game\¹Ö®Ö¢ºòÈº\Save*.lsd" ".\¹Ö®Ö¢ºòÈº\"
@@ -23,5 +24,10 @@ COPY /Y "C:\Users\mike2\AppData\Roaming\natsuno-kanata\Local Storage\leveldb\*" 
 
 DEL /Q ".\Pocket Mirror\Save*.rvdata2" 2>nul
 COPY /Y  "E:\game\Pocket Mirror ver.1.3\Pocket Mirror\Save*.rvdata2" ".\Pocket Mirror\"
+
+cd ..
+del /q save_*.rar 2>nul
+E:\cmdtool\rar.exe a -m5 -rr8 -xsave\.git save_%datetime%.rar save
+cd save
 
 pause
