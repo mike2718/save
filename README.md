a505604f6cd5a720bbcdd0a269a3910da5afbda5
Mike Akiba <mike2718@gmail.com>
2022-07-03 12:19:51 +0800

    添加存档

--- README.md
+++ README.md
@@ -19,5 +19,6 @@
 | [異形の箱庭 Lycoris](%E7%95%B0%E5%BD%A2%E3%81%AE%E7%AE%B1%E5%BA%AD%20Lycoris) | DLsite版 | 2.0.2 |
 | [雨宿バス停留所](%E9%9B%A8%E5%AE%BF%E3%83%90%E3%82%B9%E5%81%9C%E7%95%99%E6%89%80) | 下载版 | 1.05 |
 | [魔女の家MV](%E9%AD%94%E5%A5%B3%E3%81%AE%E5%AE%B6MV) | Steam版 | 1.06 |
+| [Memories Off 2nd](MemoOff2nd) | 98/ME/2000/XP版 | |
 
 有需要的话可以自行取用。
--- archive.bat
+++ archive.bat
@@ -4,9 +4,6 @@ cd /d "%~dp0"
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
 set datetime=%datetime:~0,4%_%datetime:~4,2%_%datetime:~6,2%
 
-rem del /q ..\save_*.tar.zst 2>nul
-rem git archive -v --format=tar.zst --prefix=save_%datetime%_HEAD/ -o ../save_%datetime%_HEAD.tar.zst HEAD
-
 del /q ".\¹Ö®Ö¢ºòÈº\Save*.lsd" 2>nul
 copy /y "E:\game\¹Ö®Ö¢ºòÈº\Save*.lsd" ".\¹Ö®Ö¢ºòÈº\"
 
@@ -27,7 +24,7 @@ copy /y "E:\game\Pocket Mirror ver.1.3\Pocket Mirror\Save*.rvdata2" ".\Pocket Mi
 
 cd ..
 del /q save_*.rar 2>nul
-E:\cmdtool\rar.exe a -m5 -rr8 -xsave\.git save_%datetime%.rar save
+E:\cmdtool\rar.exe a -m5 -rr10 -xsave\.git save_%datetime%.rar save
 cd save
 
 pause
