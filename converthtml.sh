160985ef04089a37b7bf52732b8682de43631d18
Mike Akiba <mike2718@gmail.com>
2022-08-10 21:01:22 +0800

    测试

--- converthtml.sh
+++ converthtml.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 rm index.html
-lua ~/dev/commonmark-spec/tools/make_spec.lua html < README.md > index.html
+lua ./tools/make_spec.lua html < README.md > index.html
 tidy -m index.html
