9e7898289b3870dc83155df6b3a03cc0b5126866
Mike Akiba <mike2718@gmail.com>
2022-09-03 04:06:17 +0800

    更新打包批处理

--- tar.cmd
+++ tar.cmd
@@ -1,33 +1,24 @@
 @echo off
 rem 本文件只支持gb2312编码
-setlocal enableextensions
 cd /d "%~dp0"
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
 set today=%MyDate:~0,4%%MyDate:~4,2%%MyDate:~6,2%
 
-del /q ..\save_*.tar.zst ..\save_*.zip
-git archive --format=tar.zst --prefix=save_HEAD_%today%/ --output=../save_HEAD_%today%.tar.zst HEAD
+del /q ..\save_*.zip
+rem git archive --format=tar.zst --prefix=save_HEAD_%today%/ --output=../save_HEAD_%today%.tar.zst HEAD
 git archive --format=zip --prefix=save_HEAD_%today%/ --output=../save_HEAD_%today%.zip HEAD
 
-del /q ".\怪異症候群\Save*.lsd"
-copy /y "E:\game\怪異症候群\Save*.lsd" ".\怪異症候群\"
+copy /y "E:\game\怪異症候群\Save*.lsd" ".\怪異症候群"
 
-del /q ".\7 Days to End with You\*.es3"
-copy /y "C:\Users\mike2\AppData\LocalLow\Lizardry\Seven Days to End with You\*.es3" ".\7 Days to End with You\"
+xcopy "C:\Users\mike2\AppData\LocalLow\Lizardry\Seven Days to End with You" ".\7 Days to End with You" /i /y
 
-del /q ".\DDLC-1454445547\*"
-copy /y "C:\Users\mike2\AppData\Roaming\RenPy\DDLC-1454445547\*" ".\DDLC-1454445547\"
+xcopy "C:\Users\mike2\AppData\Roaming\RenPy\DDLC-1454445547" ".\DDLC-1454445547" /i /y
 
-del /q ".\NEEDY GIRL OVERDOSE\Windose_Data\*.es3"
-copy /y "C:\Program Files (x86)\Steam\steamapps\common\NEEDY GIRL OVERDOSE\Windose_Data\*.es3" ".\NEEDY GIRL OVERDOSE\Windose_Data\"
+copy /y "C:\Program Files (x86)\Steam\steamapps\common\NEEDY GIRL OVERDOSE\Windose_Data\*.es3" ".\NEEDY GIRL OVERDOSE\Windose_Data"
 
-rd /s /q .\natsuno-kanata\leveldb\*
-mkdir natsuno-kanata
 xcopy C:\Users\mike2\AppData\Roaming\natsuno-kanata\* .\natsuno-kanata /s /i /y
 
 rem hololive ERROR 完全版
-rd /s /q ".\hololive ERROR\*"
-mkdir "hololive ERROR"
 xcopy "C:\Users\mike2\AppData\LocalLow\cover\hololive ERROR\*"  ".\hololive ERROR" /s /i /y
 
 rem コープスパーティーブラッドカバー５
@@ -38,7 +29,6 @@ rem adb pull /data/data/com.uniteatsushi.sokaishin/files/save.ini E:/dev/save/co
 rem adb pull /data/data/com.uniteatsushi.sokaishin/files/setting.ini E:/dev/save/com.uniteatsushi.sokaishin
 
 rem HoloCure
-mkdir HoloCure
 xcopy C:\Users\mike2\AppData\Local\HoloCure .\HoloCure /i /y
 
 rem 转换为html
