f29ba03bfb6cc221154cab2ab6a47bbda9714791
Mike Akiba <mike2718@gmail.com>
2023-02-07 01:13:58 +0800

    还原正确的存档路径

--- make.cmd
+++ make.cmd
@@ -1,13 +1,13 @@
 @echo off
 cd /d "%~dp0"
 
-xcopy C:\Users\mike2\AppData\Roaming\7DaysToDie\Saves\* E:\dev\save\7DaysToDie /s /i /y
+xcopy "C:\Users\mike2\AppData\LocalLow\Lizardry\Seven Days to End with You\*" "E:\dev\save\Seven Days to End with You" /s /i /y
 copy /y "C:\Program Files (x86)\Steam\steamapps\common\NEEDY GIRL OVERDOSE\Windose_Data\*.es3" "e:\dev\save\NEEDY GIRL OVERDOSE\Windose_Data"
 xcopy C:\Users\mike2\AppData\Roaming\natsuno-kanata\* e:\dev\save\natsuno-kanata /s /i /y
 xcopy "C:\Users\mike2\AppData\LocalLow\cover\hololive ERROR\*"  "e:\dev\save\hololive ERROR" /s /i /y
 copy /y "E:\game\TeachingFeeling-4.¦Â2\*.sav" "e:\dev\save\TeachingFeeling-4.¦Â2"
 
-del /q ..\save-main.zip
+del /q ..\save-main.zip 2>nul
 git archive --format=zip --prefix=save-main/ --output=../save-main.zip main
 
 pause
