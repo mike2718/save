e58957055c2c6cf311f6bb03029dcc8386e3d65b
Mike Akiba <mike2718@gmail.com>
2023-02-11 03:37:50 +0800

    添加Love Love School Days存档

--- make.cmd
+++ make.cmd
@@ -3,9 +3,11 @@ cd /d "%~dp0"
 
 xcopy "C:\Users\mike2\AppData\LocalLow\Lizardry\Seven Days to End with You\*" "E:\dev\save\Seven Days to End with You" /s /i /y
 copy /y "C:\Program Files (x86)\Steam\steamapps\common\NEEDY GIRL OVERDOSE\Windose_Data\*.es3" "E:\dev\save\NEEDY GIRL OVERDOSE\Windose_Data"
-xcopy C:\Users\mike2\AppData\Roaming\natsuno-kanata\* e:\dev\save\natsuno-kanata /s /i /y
-xcopy "C:\Users\mike2\AppData\LocalLow\cover\hololive ERROR\*"  "e:\dev\save\hololive ERROR" /s /i /y
-copy /y "E:\game\TeachingFeeling-4.¦Â2\*.sav" "e:\dev\save\TeachingFeeling-4.¦Â2"
+xcopy C:\Users\mike2\AppData\Roaming\natsuno-kanata\* E:\dev\save\natsuno-kanata /s /i /y
+xcopy "C:\Users\mike2\AppData\LocalLow\cover\hololive ERROR\*"  "E:\dev\save\hololive ERROR" /s /i /y
+copy /y "E:\game\TeachingFeeling-4.¦Â2\*.sav" "E:\dev\save\TeachingFeeling-4.¦Â2"
+xcopy "C:\Users\mike2\AppData\LocalLow\OTLGAME\LoveLoveSchoolDays\*" "E:\dev\save\LoveLoveSchoolDays" /s /i /y
+
 
 del /q ..\save-main.zip 2>nul
 git archive --format=zip --prefix=save-main/ --output=../save-main.zip main
