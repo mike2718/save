1049b25fea86f6307364658cb7a02d3fa266659e
Mike Akiba <mike2718@gmail.com>
2023-03-16 23:40:04 +0800

    更新批处理

--- make.cmd
+++ make.cmd
@@ -1,14 +1,14 @@
 @echo off
 cd /d "%~dp0"
 
-xcopy "C:\Users\mike2\AppData\LocalLow\Lizardry\Seven Days to End with You\*" "E:\dev\save\Seven Days to End with You" /s /i /y
-copy /y "C:\Program Files (x86)\Steam\steamapps\common\NEEDY GIRL OVERDOSE\Windose_Data\*.es3" "E:\dev\save\NEEDY GIRL OVERDOSE\Windose_Data"
-rem xcopy C:\Users\mike2\AppData\Roaming\natsuno-kanata\* E:\dev\save\natsuno-kanata /s /i /y
-xcopy "C:\Users\mike2\AppData\LocalLow\cover\hololive ERROR\*"  "E:\dev\save\hololive ERROR" /s /i /y
-copy /y "E:\game\TeachingFeeling-4.¦Â2\*.sav" "E:\dev\save\TeachingFeeling-4.¦Â2"
-xcopy "C:\Users\mike2\AppData\LocalLow\OTLGAME\LoveLoveSchoolDays\*" "E:\dev\save\LoveLoveSchoolDays" /s /i /y
+rem xcopy "C:\Users\Saori\AppData\LocalLow\Lizardry\Seven Days to End with You\*" "C:\Users\Saori\dev\save\Seven Days to End with You" /s /i /y
+rem copy /y "C:\Program Files (x86)\Steam\steamapps\common\NEEDY GIRL OVERDOSE\Windose_Data\*.es3" "C:\Users\Saori\dev\save\NEEDY GIRL OVERDOSE\Windose_Data"
+rem xcopy C:\Users\Saori\AppData\Roaming\natsuno-kanata\* C:\Users\Saori\dev\save\natsuno-kanata /s /i /y
+rem xcopy "C:\Users\Saori\AppData\LocalLow\cover\hololive ERROR\*"  "C:\Users\Saori\dev\save\hololive ERROR" /s /i /y
+rem copy /y "C:\Users\Saori\game\TeachingFeeling-4.¦Â2\*.sav" "C:\Users\Saori\dev\save\TeachingFeeling-4.¦Â2"
+rem xcopy "C:\Users\Saori\AppData\LocalLow\OTLGAME\LoveLoveSchoolDays\*" "C:\Users\Saori\dev\save\LoveLoveSchoolDays" /s /i /y
 
-type README.md | awk "/^\|/" | C:\msys64\usr\bin\sort.exe > README-sorted.md
+type README.md | awk "/^\|/" | sort.exe > README-sorted.md
 
 del /q ..\save-main.zip 2>nul
 git archive --format=zip --prefix=save-main/ --output=../save-main.zip main
