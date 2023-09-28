d3011ed8be97d3b1d2520b788ba1f5c1a4aa436e
Mike Akiba <mike2718@gmail.com>
2023-09-29 03:01:29 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git archive --format=tar.zst --prefix=save/ --output=/d/save.tar.zst main
+git archive --format=tar.xz --prefix=save/ --output=/d/save.tar.xz main
 rm README-sorted.md
 cat README.md | awk '/^\|/' | sort > README-sorted.md
