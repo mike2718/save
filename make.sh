e87c83fc68a5c765c833e3beac71d6a3bb7965cb
mike <237797+mike2718@users.noreply.github.com>
2025-12-21 03:25:32 +0800

    update

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 git archive --format=zip --prefix=save/ --output=/c/Users/Saori/backup/save.zip main
 
-cat README.md | awk '/^\|/' | sort > README-sorted.md
-cat save_path.md | awk '/^\|/' | sort > save_path-sorted.md
+#cat README.md | awk '/^\|/' | sort > README-sorted.md
+#cat save_path.md | awk '/^\|/' | sort > save_path-sorted.md
 
-git add .gitattributes .gitignore make.sh README.md save_path.md
+#git add .gitattributes .gitignore make.sh README.md save_path.md
