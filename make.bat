88248352fd2032da6f961cb5d7e7d2e9d8055f99
mike <237797+mike2718@users.noreply.github.com>
2025-12-21 03:10:37 +0800

    update

--- make.bat
+++ make.bat
@@ -9,4 +9,5 @@ rem xcopy "C:\Users\Saori\AppData\LocalLow\OTLGAME\LoveLoveSchoolDays\*" ".\Love
 xcopy "C:\Users\Saori\Saved Games\FALCOM\ZWEI2P\*" ".\ZWEI2P" /s /i /y
 xcopy "C:\Users\Saori\AppData\LocalLow\AIHASTO\MiSideFull\*" ".\MiSideFull" /s /i /y
 xcopy "C:\Program Files (x86)\Steam\userdata\323136698\1794680\remote\*" ".\Vampire_Survivors" /s /i /y
+xcopy "C:\Users\Saori\AppData\Local\Dysthanasia\Saved\SaveGames\*.sav" ".\Dysthanasia" /s /i /y
 
